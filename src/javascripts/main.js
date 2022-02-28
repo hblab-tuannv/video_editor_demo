5d2f93a50f6c689be371a8e657fe6f77d241f4c9
hblab-tuannv <tuannv@hblab.vn>
2022-02-28 15:16:30 +0700

    [feature] convert video type to video/mp4

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -13,8 +13,27 @@ document.onreadystatechange = function () {
   });
 
   document.getElementById('video_input').addEventListener('change', async (e) => {
+    let videoFile = e.target.files[0];
+
+    if (videoFile.type != 'video/mp4') {
+      const { createFFmpeg } = FFmpeg;
+      const ffmpeg = createFFmpeg({
+        corePath: '../../static/js/ffmpeg-core.js',
+        log: true,
+      });
+
+      const file = new Uint8Array(await readFromBlobOrFile(videoFile));
+      if (!ffmpeg.isLoaded()) {
+        await ffmpeg.load();
+      }
+      ffmpeg.FS('writeFile', videoFile.name, file);
+      await ffmpeg.run('-i', videoFile.name, '-c:v', 'libx264', 'output.mp4');
+      const data = ffmpeg.FS('readFile', 'output.mp4');
+      videoFile = new Blob([data.buffer], { type: 'video/mp4' });
+    }
+
     let video = document.getElementById('video_preview');
-    video.src = URL.createObjectURL(e.target.files[0]);
+    video.src = URL.createObjectURL(videoFile);
     video.controls = true
 
     let videoTrackCanvas = new VideoTrackCanvas(e.target.files[0], 20, 35).create();
@@ -23,3 +42,16 @@ document.onreadystatechange = function () {
     let fabricVideo = await new VideoRender(fabricCanvas, video).render();
   });
 };
+
+const readFromBlobOrFile = (blob) => (
+  new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = ({ target: { error: { code } } }) => {
+      reject(Error(`File could not be read! Code=${code}`));
+    };
+    fileReader.readAsArrayBuffer(blob);
+  })
+);
